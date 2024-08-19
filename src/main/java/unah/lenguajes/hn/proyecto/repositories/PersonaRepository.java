1c0520510811e9ffeb0b1acd7e26e271426e6978
Pablo Interiano <interianoppablo@gmail.com>
2024-08-19 02:13:34 -0600

    login Usuario

--- src/main/java/unah/lenguajes/hn/proyecto/repositories/PersonaRepository.java
+++ src/main/java/unah/lenguajes/hn/proyecto/repositories/PersonaRepository.java
@@ -2,8 +2,13 @@ package unah.lenguajes.hn.proyecto.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 import unah.lenguajes.hn.proyecto.models.Persona;
 
 public interface PersonaRepository extends JpaRepository<Persona, String> {
     
+    public Boolean existsByCorreo(String correo);
+    
+    public Persona findByCorreo(String correo);
 }
